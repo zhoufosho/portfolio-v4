d7942b4d88f0027ee54068eb53c390c6d2e6187c
Elaine Zhou <ezhou@stanford.edu>
2016-02-18 06:13:10 -0800

    hide unfinished projects

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ var router = express.Router();
 var projects = [{name: "discoveroute", slug:"discoveroute", img:"images/discoveroute3.png", content: "discoveroute is a mobile web app for discovering path-based sites along routes."},
 				{name: "Dais Glass", slug:"dais", img:"images/dais.png", content: "Dais is a Google Glass presentation aid & analytics tool."},
 				{name: "Research", slug:"research", img: "images/dconstraints.png", content: "Here is independent research on delaying design constraints."},
-				{name: "SmartCart", slug:"smartcart", img:"images/smartcart3.png", content: "SmartCart is a smart shopping tablet app that helps users to quickly locate items and purchase them at the cart."},
-				{name: "Six Degree", slug:"sixdegree", img:"images/sixdegr.png", content: "SixDegree is a conceptual media tool that weights your relationships."},
-				{name: "Respiration", slug:"respiration", img:"images/respiration2.png", content: "Respiration is an interactive art piece on city life and its people."}];
+				{name: "SmartCart", slug:"smartcart", img:"images/smartcart3.png", content: "SmartCart is a smart shopping tablet app that helps users to quickly locate items and purchase them at the cart."}];
+				// {name: "Six Degree", slug:"sixdegree", img:"images/sixdegr.png", content: "SixDegree is a conceptual media tool that weights your relationships."},
+				// {name: "Respiration", slug:"respiration", img:"images/respiration2.png", content: "Respiration is an interactive art piece on city life and its people."}];
 
 /* GET home page. */
 router.get('/', function(req, res) {

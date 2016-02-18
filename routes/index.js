607f59624e7109a422f384bbbba2453ea69ba96b
Elaine Zhou <ezhou@stanford.edu>
2016-02-18 06:09:15 -0800

    pseudo homepage

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var projects = [{name: "discoveroute", slug:"discoveroute", img:"images/discoveroute3.png", content: "discoveroute is a mobile web app for discovering path-based sites along routes. Find new places by old spaces."},
-				{name: "Dais Glass", slug:"dais", img:"images/daisglass.png", content: "Dais is a Google Glass presentation aid & analytics tool."},
-				{name: "Research", slug:"research", img: "images/bdp.png", content: "Here is independent research on delaying design constraints."},
-				{name: "SmartCart", slug:"smartcart", img:"images/smartcart.png", content: "SmartCart is a smart shopping tablet app that helps users to quickly locate items and purchase them at the cart."},
+var projects = [{name: "discoveroute", slug:"discoveroute", img:"images/discoveroute3.png", content: "discoveroute is a mobile web app for discovering path-based sites along routes."},
+				{name: "Dais Glass", slug:"dais", img:"images/dais.png", content: "Dais is a Google Glass presentation aid & analytics tool."},
+				{name: "Research", slug:"research", img: "images/dconstraints.png", content: "Here is independent research on delaying design constraints."},
+				{name: "SmartCart", slug:"smartcart", img:"images/smartcart3.png", content: "SmartCart is a smart shopping tablet app that helps users to quickly locate items and purchase them at the cart."},
 				{name: "Six Degree", slug:"sixdegree", img:"images/sixdegr.png", content: "SixDegree is a conceptual media tool that weights your relationships."},
 				{name: "Respiration", slug:"respiration", img:"images/respiration2.png", content: "Respiration is an interactive art piece on city life and its people."}];
 

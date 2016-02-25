777aef0e944abe18abdd869a040529d0e675333e
Elaine Zhou <ezhou@stanford.edu>
2016-02-24 16:17:37 -0800

    remove links to work sites

--- public/javascripts/index-script.js
+++ public/javascripts/index-script.js
@@ -1,12 +1,12 @@
 $(document).on('ready', function(){
 
-  $('#work .tiles .tile').click(function(e) {
-    e.preventDefault();
-    var element = document.getElementById(this.id);
-  	var url = element.dataset.url;
-  	newPage(url);
+  // $('#work .tiles .tile').click(function(e) {
+  //   e.preventDefault();
+  //   var element = document.getElementById(this.id);
+  // 	var url = element.dataset.url;
+  // 	newPage(url);
 
-  });
+  // });
 
 
   $('#projects .tiles .tile').click(function(e) {
